428c1411128829ffd2eaf032f3aba74ba9bc0165
Bhumika-Baghel <bhumikabaghel15@gmail.com>
2026-08-30 20:52:36 +0530

    Add

---
+++ 02_Conditional_Statement/greatest_of_three.py
@@ -0,0 +1,12 @@
+a = int(input("enter a number a: "))
+b = int(input("enter a number b: "))
+c = int(input("enter a number c: "))
+
+if(a>=b and a>=c):
+    print("a is greater number")
+
+elif(b>=a and b>=c):
+    print("b is greater number")
+    
+else:
+    print("c is greater number")
