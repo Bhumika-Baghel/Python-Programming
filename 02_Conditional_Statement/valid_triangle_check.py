eb097e597b83cccc6d22e789228fc1af2e2ead02
Bhumika-Baghel <bhumikabaghel15@gmail.com>
2026-09-12 22:52:58 +0530

    Add

---
+++ 02_Conditional_Statement/valid_triangle_check.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the 1st angle: "))
+b = int(input("Enter the 2nd angle: "))
+c = int(input("Enter the 3rd angle: "))
+
+if a > 0 and b > 0 and c > 0 and (a + b + c == 180):
+    print("Valid Triangle")
+else:
+    print("Invalid Triangle")
