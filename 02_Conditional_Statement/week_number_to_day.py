b55d7d31a1c38cca7ce44f82fe64c4dc2bf90c30
Bhumika-Baghel <bhumikabaghel15@gmail.com>
2026-09-14 20:46:43 +0530

    Add

---
+++ 02_Conditional_Statement/week_number_to_day.py
@@ -0,0 +1,25 @@
+week = int(input("Enter week number between 1 and 7: "))
+
+if week == 1:
+    print("Monday")
+
+elif week == 2:
+    print("Tuesday")
+
+elif week == 3:
+    print("Wednesday")
+
+elif week == 4:
+    print("Thursday")
+
+elif week == 5:
+    print("Friday")
+
+elif week == 6:
+    print("Saturday")
+
+elif week == 7:
+    print("Sunday")
+
+else:
+    print("Enter a number between 1 and 7.")
